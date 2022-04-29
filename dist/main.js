5d6fca776068f76cbd3b1ca20dda4d0df45c42f5
Mohamed Ashraf <botoashraf123@gmail.com>
2022-04-29 18:15:18 +0200

    build mobile menu, about us & features section

--- dist/main.js
+++ dist/main.js
@@ -11,10 +11,42 @@ window.onscroll = function() {
 // Toggle Dark Mode
 const switchModeToggler = document.getElementById('switch');
 switchModeToggler.addEventListener('click', function() {
+  document.querySelector('html').classList.add('dark')
   if(switchModeToggler.checked) {
     document.querySelector('html').classList.add('dark')
   } else {
     document.querySelector('html').classList.remove('dark')
   }
-})
+});
+
+//Open mobile menu on clicking Menu Toggler button
+const  menuToggler = document.querySelector('#menu-toggler');
+const  mobileMenu  = document.querySelector('.nav-wrapper');
+menuToggler.addEventListener('click', function() {
+  //add class active to menu toggler
+  this.classList.add('active');
+  // open mobile menu
+  document.querySelector('.nav-wrapper').classList.add('show')
+});
+
+// [ 1 ] Close Mobile Menu on clicking the close icon
+document.querySelector('#close').addEventListener('click', function() {
+  this.parentElement.parentElement.classList.remove('show');
+  menuToggler.classList.remove('active')
+});
+// [ 2 ] Close the menu on clicking any link
+document.querySelectorAll('.nav-links li a').forEach((link) => {
+  link.addEventListener('click', function() {
+    mobileMenu.classList.remove('show');
+    menuToggler.classList.remove('active')
+  })
+});
+// [ 3 ] close the menu on clicking anywhere except the menu
+mobileMenu.addEventListener('click', function (e) {
+  this.classList.remove('show');
+  menuToggler.classList.remove('active')
+});
+document.querySelector('.nav-wrapper .nav').addEventListener('click', function(e) {
+    e.stopPropagation();
+  });
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
       colors: {
         'slate-900'  : '#050748',
         'slate-600'  : '#6a6a8e',
+        'slate-500'  : '#a6afbd',
         'slate-200'  : '#e6e6e6',
         'gray-400'   : '#a2a2a2',
         'pink-600'   : '#e60072',
@@ -18,15 +19,18 @@ module.exports = {
         openSans: ['Open Sans', 'sans-serif'],
       },
       backgroundImage: {
-        'sun'          : 'url("../dist/images/sun.svg")',
-        'moon'         : 'url("../dist/images/moon.svg")',
-        'gradient'  : 'url("../dist/images/bg-gradient.jpg")',
-        'blue-gradient': 'linear-gradient(to right, #673AB7 0%,#2196F3 100%)',
-        'red-gradient': 'linear-gradient(to right, #f92c8b 0%,#b02cd6 100%)',
-        'heading-bg'   : 'linear-gradient(to right, #673AB7 0%,#E91E63 36%,#E91E63 65%,#673AB7 100%)' 
+        'sun'              : 'url("../dist/images/sun.svg")',
+        'moon'             : 'url("../dist/images/moon.svg")',
+        'hero-gradient'         : 'url("../dist/images/bg-gradient.jpg")',
+        'blue-gradient'    : 'linear-gradient(to right, #673AB7 0%,#2196F3 100%)',
+        'red-gradient'     : 'linear-gradient(to right, #f92c8b 0%,#b02cd6 100%)',
+        'heading-gradient' : 'linear-gradient(to right, #673AB7 0%,#E91E63 36%,#E91E63 65%,#673AB7 100%)',
+        'section-gradient' :  'linear-gradient(to bottom, #fff6f3 0%,#fffefb 100%)',
+        'pseudo-gradient'  :  'linear-gradient(to right, #007bff 0%,#ff1f8e 100%)'
       },
       boxShadow: {
-        'btn': '0 10px 15px 0px rgb(56 0 189 / 20%)',
+        'btn'  : '0 10px 15px 0px rgb(56 0 189 / 20%)',
+        'card' : '0 3.4px 2.7px -30px rgba(0, 0, 0, 0.059), 0 8.2px 8.9px -30px rgba(0, 0, 0, 0.071), 0 25px 40px -30px rgba(0, 0, 0, 0.2)'
       },
       zIndex: {
         '9999': 'z-index: 9999'
